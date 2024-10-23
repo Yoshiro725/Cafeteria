88f23fcef4d5a03e3f341182821faef3afbe382f
Rafita2404 <rafael.bravo63@unach.mx>
2024-10-22 22:22:53 -0600

    Add files via upload

---
+++ Almacenamiento.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package Modelo;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author user
+ */
+public interface Almacenamiento {
+        
+    public void Create();
+    
+    public ArrayList Read();
+    
+    public void Update();
+    
+    public void Delete();
+    
+}
